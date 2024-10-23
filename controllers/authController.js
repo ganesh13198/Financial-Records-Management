93c6bc84d0b5faf387ebbda6e8bf82de703d76a7
ganesh13198 <ganesh.santosh200@gmail.com>
2024-10-23 16:14:30 +0530

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,49 @@
 import User from '../models/User.js';
 import jwt from 'jsonwebtoken';
+import validator from 'validator';
 
-export const register = async (req, res) => {
+export const register = async (req,res) => {
+
     try {
-        const { username, password, email } = req.body;
-        const user = new User({ username, password, email });
-        await user.save();
-        res.status(201).json({ message: 'User registered successfully' });
+        
+        const {username, email, password} = req.body;
+
+        // checking user already exists or not
+        const exists = await User.findOne({email});
+        if (exists) {
+            return res.json({success:false, message: "User already exists"})
+        }
+
+        // validatin email fomate & strong password
+        if (!validator.isEmail(email)) {
+            return res.json({success:false, message: "Please enter a valid email"})
+        }
+        if (password.length < 4) {
+            return res.json({success:false, message: "Please enter strong password"})
+        }
+
+        // hashing user password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        // create new user
+        const newUser = new userModel({
+            username,
+            email,
+            password:hashedPassword
+        })
+
+        const user = await newUser.save()
+
+        const token = createToken(user._id)
+        res.json({success:true,token})
+
     } catch (error) {
-        res.status(400).json({ error: error.message });
+        console.log(error);
+        res.json({success:false,message:error.message})
+        
     }
-};
+}
 
 export const login = async (req, res) => {
     try {
